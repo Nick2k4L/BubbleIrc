cbb5662e1df7071aaa01f5f36a6f253b55faf06a
Nicholas Lopez <nico2k4@gmail.com>
2026-08-06 13:13:13 -0700

    Update: `main.go` & `go.mod`

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	tea "charm.land/bubbletea/v2"
+	"github.com/Nick2k4L/BubbleIrc/models"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	p := tea.NewProgram(models.InitiateApp())
+	if _, err := p.Run(); err != nil {
+		fmt.Printf("Alas, there's been an error: %v", err)
+		os.Exit(1)
+	}
 }
