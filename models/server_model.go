a62d657d570211ed97cc3ada5e6d5b85f02b1ecd
Nicholas Lopez <nico2k4@gmail.com>
2026-08-07 10:18:39 -0700

    little serverModel test

--- models/server_model.go
+++ models/server_model.go
@@ -1,3 +1,23 @@
 package models
 
-type ServerModel struct {}
+type ServerModel struct {
+	test []string
+}
+
+// These would be some type of box? With highlighting
+// once on enter, we will be able to go back. Backspace
+// will allow users to go back to the server list
+func InitServerModel() *ServerModel {
+	return &ServerModel{
+		test: []string{"Server1", "Server2", "Server3"},
+	}
+}
+
+func (m *ServerModel) View() string {
+	var final string
+	for _, server := range m.test {
+		final += server + "\n"
+	}
+
+	return final
+}
