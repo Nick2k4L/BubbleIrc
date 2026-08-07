bddce90d379a05f533acd0fcb797f99e265dc122
Nicholas Lopez <nico2k4@gmail.com>
2026-08-07 10:18:26 -0700

    Update: take in new parameters

--- components/boxstyle.go
+++ components/boxstyle.go
@@ -1,29 +1,45 @@
 package components
 
-import "charm.land/lipgloss/v2"
+import (
+	"charm.land/lipgloss/v2"
+	"github.com/Nick2k4L/BubbleIrc/theme"
+)
 
 var (
-	Pink   = "#fd4be8"
-	Teal   = "#4bfde5"
-	Orange = "#fd794b"
+	Pink            = "#fd4be8"
+	Teal            = "#4bfde5"
+	Orange          = "#fd794b"
+	BackgroundColor = "#ffffff"
 )
 
 // BoxOptions. This allows to create custom boxes / borders as we go on
 // With things such as themes in mind, this will come in hand
 type BoxOptions struct {
-	Color   string
+	// Color                string
 	Padding []int
 	Width   int
 	Height  int
-	Title   string
+	// Title                string
+	// BackgroundColor      color.Color
+	IsCenteredHorizontal bool
+	IsSubBox             bool
 }
 
-// CreateNewRoundedBoxStyle will create a new rounded box
-func CreateNewRoundedBoxStyle(boxOptions BoxOptions) lipgloss.Style {
+// CreateNewRoundedBoxStyle will create a new rounded box -- boxOptions should eventually consume theme
+func CreateNewRoundedBoxStyle(content string, boxOptions BoxOptions, theme theme.Theme) string {
 	// rounded box
 	boxStyle := lipgloss.NewStyle().
-		Border(lipgloss.RoundedBorder()).
-		BorderForeground(lipgloss.Color(boxOptions.Color))
+		Border(lipgloss.RoundedBorder()) // we want this to be a rounded box -- keep this as is
+
+	if !boxOptions.IsSubBox {
+		boxStyle = boxStyle.BorderForeground(lipgloss.Color(theme.MainBorderColor))
+	} else {
+		boxStyle = boxStyle.BorderForeground(lipgloss.Color(theme.SubBorderColor))
+	}
+
+	if boxOptions.IsCenteredHorizontal {
+		boxStyle = boxStyle.AlignHorizontal(lipgloss.Center)
+	}
 
 	// we check if any of these options are set, if they are not move on
 	if len(boxOptions.Padding) > 0 {
@@ -38,5 +54,9 @@ func CreateNewRoundedBoxStyle(boxOptions BoxOptions) lipgloss.Style {
 		boxStyle = boxStyle.Height(boxOptions.Height)
 	}
 
-	return boxStyle
+	if theme.BackgroundColor != "" {
+		boxStyle = boxStyle.Background(lipgloss.Color(theme.BackgroundColor)).BorderBackground(lipgloss.Color(theme.BackgroundColor))
+	}
+
+	return boxStyle.Render(content)
 }
