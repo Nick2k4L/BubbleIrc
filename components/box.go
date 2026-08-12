7a90f7c5ce8450704c243b638a3a5df529592458
Nicholas Lopez <nico2k4@gmail.com>
2026-08-12 12:22:54 -0700

    Update: box.go way better now

--- components/box.go
+++ components/box.go
@@ -6,79 +6,42 @@ import (
 	"github.com/charmbracelet/x/exp/charmtone"
 )
 
-// REFACTOR THIS -- THIS IS BAD AS WELL....
-
-type Box struct {
-	Width                int
-	Height               int
-	Padding              []int
-	IsCenteredHorizontal bool
-	IsSubBox             bool
-	IsBlend              bool
-	Theme                theme.Theme
-	Content              string
-	PopUpContent         string
-
-	style lipgloss.Style
-}
-
-// Apply all the themeing props we need
-func (b *Box) applyTheme() {
-	// Start with a fresh base style
-	b.style = lipgloss.NewStyle().Border(lipgloss.RoundedBorder())
-
-	if !b.IsSubBox {
-		b.style = b.style.BorderForeground(lipgloss.Color(b.Theme.MainBorderColor))
-	} else {
-		b.style = b.style.BorderForeground(lipgloss.Color(b.Theme.SubBorderColor))
+// Use styles
+func MainBoxStyle(th theme.Theme) lipgloss.Style {
+	style := lipgloss.NewStyle().
+		Border(lipgloss.RoundedBorder()).
+		BorderForeground(lipgloss.Color(th.MainBorderColor))
+	if th.BackgroundColor != "" {
+		style = style.Background(lipgloss.Color(th.BackgroundColor)).
+			BorderBackground(lipgloss.Color(th.BackgroundColor))
 	}
 
-	// this will need to change in the future....
-	if b.IsBlend {
-		b.style = b.style.BorderForegroundBlend(
+	return style
+}
+
+func SubBoxStyle(th theme.Theme) lipgloss.Style {
+	return MainBoxStyle(th).
+		BorderForeground(lipgloss.Color(th.SubBorderColor)).
+		Width(35)
+}
+
+func PopUpBoxStyle(th theme.Theme) lipgloss.Style {
+	return MainBoxStyle(th).
+		BorderForegroundBlend(
 			charmtone.Cherry,
 			charmtone.Charple,
 			charmtone.Guac,
 			charmtone.Charple,
 			charmtone.Sriracha,
 		)
-	}
-
-	if b.IsCenteredHorizontal {
-		b.style = b.style.AlignHorizontal(lipgloss.Center)
-	}
-
-	if b.Theme.BackgroundColor != "" {
-		b.style = b.style.Background(lipgloss.Color(b.Theme.BackgroundColor)).
-			BorderBackground(lipgloss.Color(b.Theme.BackgroundColor))
-	}
 }
 
-// apply width & sizing
-func (b *Box) applySizing() {
-	if len(b.Padding) > 0 {
-		b.style = b.style.Padding(b.Padding...)
-	}
-	if b.Width > 0 {
-		b.style = b.style.Width(b.Width)
-	}
-	if b.Height > 0 {
-		b.style = b.style.Height(b.Height)
-	}
-}
-
-func (b *Box) Render() string {
-	b.applyTheme()
-	b.applySizing()
-	return b.style.Render(b.Content)
-}
-
-func (b *Box) RenderPopUp(mainBox string) string {
-	popup := lipgloss.NewLayer(b.Render())
+func RenderPopUp(popUpBox, mainBox string, width, height int) string {
+	popup := lipgloss.NewLayer(popUpBox)
 
 	// Use the actual app UI as the base layer so the canvas matches the terminal size
 	baseLayer := lipgloss.NewLayer(mainBox).AddLayers(
-		popup.X(b.Width).Y(b.Height).Z(1),
+		popup.X(width).Y(height).Z(1),
 	)
 
 	comp := lipgloss.NewCompositor(baseLayer)
